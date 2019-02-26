fe4c5f8ed48ff1096f5f0537fdb608bcd3d8f1d9
Adelin Nedelcu <n.adelin0223@gmail.com>
2019-02-26 21:56:21 +0200

    Final Code
    Added the code for all possible movements of the car, also added a second ultrasonic sensor

--- obstacle_avoiding_car.ino
+++ obstacle_avoiding_car.ino
@@ -1,70 +1,100 @@
-const int leftForward = 2;
-const int leftBackward = 3;
-const int rightForward = 5;
-const int rightBackward = 4;
+const int leftForward = 5;
+const int leftBackward = 4;
+const int rightForward = 2;
+const int rightBackward = 3;
 
-const int echoPin = 10;
-const int trigPin = 9;
+const int echoPinForwardSensor = 10;
+const int trigPinForwardSensor = 9;
+const int echoPinLeftSensor = 6;
+const int trigPinLeftSensor = 11;
 
-int fastSpeed = 130, slowSpeed = 50, balanceValue = 125;
-int distance, duration, distanceWhenStops = 30, randomNumber;
+int fastSpeed = 205, balanceValue = 50;
+int distanceForwardSensor, distanceLeftSensor, durationLeftSensor, durationForwardSensor, distanceWhenStops = 35, distanceToGoBackwards = 15;
 
 bool stopped = false;
 
-uint32_t durationToTurn = 300;
+uint32_t durationToTurn = 1200;
 
-void setup() {
+void setup () 
+{
   pinMode(leftForward, OUTPUT);
   pinMode(leftBackward, OUTPUT);
   pinMode(rightForward, OUTPUT);
   pinMode(rightBackward, OUTPUT);
 
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
+  pinMode(trigPinForwardSensor, OUTPUT);
+  pinMode(echoPinForwardSensor, INPUT);
+  pinMode(trigPinLeftSensor, OUTPUT);
+  pinMode(echoPinLeftSensor, INPUT);
 
-  //randomSeed(analogRead(0));
-  //Serial.begin(9600);
+  Serial.begin(9600);
 }
 
-void loop() {
-  digitalWrite(trigPin, LOW);
+void loop () 
+{
+  //Calculates the distance given by the front sensonr
+  digitalWrite(trigPinForwardSensor, LOW);
+  delayMicroseconds(2);
+  
+  digitalWrite(trigPinForwardSensor, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPinForwardSensor, LOW);
+
+  durationForwardSensor = pulseIn(echoPinForwardSensor, HIGH);
+  distanceForwardSensor = durationForwardSensor * 0.034 / 2; 
+
+  //Calculates the distance given by the left side sensor
+  digitalWrite(trigPinLeftSensor, LOW);
   delayMicroseconds(2);
 
-  digitalWrite(trigPin, HIGH);
+  digitalWrite(trigPinLeftSensor, HIGH);
   delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
+  digitalWrite(trigPinLeftSensor, LOW);
 
-  duration = pulseIn(echoPin, HIGH);
-  distance= duration*0.034/2; 
+  durationLeftSensor = pulseIn(echoPinLeftSensor, HIGH);
+  distanceLeftSensor = durationLeftSensor * 0.034 / 2;
 
-  //Serial.print("Distanta: ");
-  //Serial.println(distance);
-
-  if (distance > distanceWhenStops) 
+  if (distanceForwardSensor <= distanceToGoBackwards && distanceForwardSensor > 0)
+  {
+    goBackwards();
+  }
+  else if (distanceForwardSensor > distanceWhenStops) 
   {
     goForward();
   }
-  else if (distance <= distanceWhenStops && stopped == false)
+  else if (distanceForwardSensor <= distanceWhenStops && stopped == false)
   {
     stopCar();
-    delay(500);
+    delay(300);
     stopped = true;
   }
   if (stopped ==  true)
   {
-    for (uint32_t timeStart = millis(); (millis() - timeStart) < durationToTurn; )
+    if (distanceLeftSensor <= distanceWhenStops && distanceLeftSensor > 0)
     {
-      turnLeft();  
+      //the following for moves the car for the amount of miliseconds given by the 'durationToTurn' variable
+      for (uint32_t timeStart = millis(); (millis() - timeStart) < durationToTurn; )
+      {
+        turnRight();  
+      }
     }
+    else
+    {
+      for (uint32_t timeStart = millis(); (millis() - timeStart) < durationToTurn; )
+      {
+        turnLeft();  
+      }
+    }
+    
     stopped = false;
   }
 }
 
 void goForward ()
 {
-  analogWrite(leftForward, fastSpeed);
+  analogWrite(leftForward, fastSpeed +  balanceValue);
   digitalWrite(leftBackward, LOW);
-  analogWrite(rightForward, fastSpeed + balanceValue);
+  analogWrite(rightForward, fastSpeed);
   digitalWrite(rightBackward, LOW);
 }
 
@@ -80,14 +110,21 @@ void turnLeft ()
 {
   digitalWrite(leftForward, LOW);
   digitalWrite(leftBackward, LOW);
-  digitalWrite(rightForward, fastSpeed + balanceValue);
+  digitalWrite(rightForward, fastSpeed);
   digitalWrite(rightBackward, LOW);
 }
 void turnRight ()
 {
-  digitalWrite(leftForward, fastSpeed + balanceValue);
+  digitalWrite(leftForward, fastSpeed);
   digitalWrite(leftBackward, LOW);
   digitalWrite(rightForward, LOW);
   digitalWrite(rightBackward, LOW);
-  
+}
+
+void goBackwards ()
+{
+  digitalWrite(leftForward, LOW);
+  digitalWrite(leftBackward, fastSpeed);
+  digitalWrite(rightForward, LOW);
+  digitalWrite(rightBackward, fastSpeed);
 }
